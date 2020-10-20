8fd73e083ac6534c1ddb4dc4e52cb3c776255200
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-20 17:58:11 -0500

    created employee, eng, intern, mgr js files

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+
+    constructor(name, id, email, officeNum) {
+
+        super(name, id, email);
+
+        this.officeNumber = officeNum;
+    }
+    getOfficeNum(){
+        
+        return this.officeNumber; 
+    }
+    getRole(){
+        return "Manager"
+    }
+}
+
+module.exports = Manager;
